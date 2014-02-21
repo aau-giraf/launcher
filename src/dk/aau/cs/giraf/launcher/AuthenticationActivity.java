11e6cb3d8c1adca3593794b40d2e78a3f4dd90f8
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2014-02-21 10:21:49 +0100

    Added new functionality to simulate the AnimationDrawable android class.
    AnimationDrawable has the bad feature of loading all images contained in the animation into memory all at once, and the converting them to bitmaps where each image will in (maybe only worst case) take up 4 bytes per pixel.
    
    Earlier use of AnimationDrawable resulted in an "outofmemery" error.

--- src/dk/aau/cs/giraf/launcher/AuthenticationActivity.java
+++ src/dk/aau/cs/giraf/launcher/AuthenticationActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.zxing.Result;
