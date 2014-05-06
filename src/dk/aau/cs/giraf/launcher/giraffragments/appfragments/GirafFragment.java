ef137e989f4c3ee3a19f2454c53f96ffc3184b7e
Tonaplo <tonaplo@msn.com>
2014-05-06 13:47:39 +0200

    added functionality to add and remove apps from profiles

--- src/dk/aau/cs/giraf/launcher/giraffragments/appfragments/GirafFragment.java
+++ src/dk/aau/cs/giraf/launcher/giraffragments/appfragments/GirafFragment.java
@@ -1,24 +1,58 @@
 package dk.aau.cs.giraf.launcher.giraffragments.appfragments;
 
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.util.HashMap;
 import java.util.List;
 
 import dk.aau.cs.giraf.launcher.R;
+import dk.aau.cs.giraf.launcher.activities.HomeActivity;
+import dk.aau.cs.giraf.launcher.activities.ProfileLauncher;
+import dk.aau.cs.giraf.launcher.helper.Constants;
 import dk.aau.cs.giraf.launcher.helper.LauncherUtility;
 import dk.aau.cs.giraf.launcher.layoutcontroller.AppInfo;
+import dk.aau.cs.giraf.oasis.lib.Helper;
+import dk.aau.cs.giraf.oasis.lib.controllers.ApplicationController;
+import dk.aau.cs.giraf.oasis.lib.controllers.ProfileApplicationController;
 import dk.aau.cs.giraf.oasis.lib.models.Application;
+import dk.aau.cs.giraf.oasis.lib.models.Profile;
+import dk.aau.cs.giraf.oasis.lib.models.ProfileApplication;
 
 /**
  * Created by Vagner on 01-05-14.
  */
 public class GirafFragment extends AppFragment{
+
+    private HashMap<String,AppInfo> appInfos;
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ApplicationController ac = new ApplicationController(context);
+            ProfileApplicationController pac = new ProfileApplicationController(context);
+            Profile user = LauncherUtility.findCurrentUser(context);
+            List<Application> userAppList = ac.getApplicationsByProfile(user);
+            AppInfo app = appInfos.get(v.getTag().toString());
+
+            if(userAppList.contains(app.getApp()))
+            {
+                pac.removeProfileApplicationByProfileAndApplication(app.getApp(),user);
+            }
+            else
+            {
+                ProfileApplication pa = new ProfileApplication(user.getId(), app.getApp().getId());
+                pac.insertProfileApplication(pa);
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,9 +94,10 @@ public class GirafFragment extends AppFragment{
     @Override
     public void loadApplications()
     {
+
         if (loadedApps == null || loadedApps.size() != apps.size()){
             //Remember that the apps have been added, so they are not added again by the listener
-            HashMap<String,AppInfo> appInfos = LauncherUtility.loadGirafApplicationsIntoView(context, (List<Application>) apps, appView, 110);
+            appInfos = LauncherUtility.loadGirafApplicationsIntoView(context, (List<Application>) apps, appView, 110, listener);
             if (appInfos == null){
                 haveAppsBeenAdded = false;
             }
@@ -72,4 +107,5 @@ public class GirafFragment extends AppFragment{
         }
         loadedApps = apps;
     }
+
 }
--- src/dk/aau/cs/giraf/launcher/helper/LauncherUtility.java
+++ src/dk/aau/cs/giraf/launcher/helper/LauncherUtility.java
@@ -437,7 +437,7 @@ public class LauncherUtility {
         return helper;
     }
 
-    public static HashMap<String,AppInfo> loadGirafApplicationsIntoView(Context context, List<Application> girafAppsList, LinearLayout targetLayout, int iconSize) {
+    public static HashMap<String,AppInfo> loadGirafApplicationsIntoView(Context context, List<Application> girafAppsList, LinearLayout targetLayout, int iconSize, View.OnClickListener listener) {
         //Get the list of apps to show in the container
         //List<Application> girafAppsList = LauncherUtility.getAvailableGirafAppsButLauncher(mContext);
         HashMap<String, AppInfo> appInfos = null;
@@ -475,7 +475,7 @@ public class LauncherUtility {
                     targetLayout.addView(currentAppRow);
                 }
 
-                ImageView newAppView = createGirafLauncherApp(context, entry.getValue(), targetLayout);
+                ImageView newAppView = createGirafLauncherApp(context, entry.getValue(), targetLayout, listener);
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(iconSize, iconSize);
                 params.weight = 1f;
                 newAppView.setLayoutParams(params);
@@ -614,7 +614,7 @@ public class LauncherUtility {
      * @param targetLayout
      * @return
      */
-    private static ImageView createGirafLauncherApp(Context context, AppInfo appInfo, LinearLayout targetLayout) {
+    private static ImageView createGirafLauncherApp(Context context, AppInfo appInfo, LinearLayout targetLayout, View.OnClickListener listener) {
         ImageView appImageView = new ImageView(context);
         final Profile currentUser = LauncherUtility.findCurrentUser(context);
         View appView = addContentToView(context, targetLayout, appInfo.getName(), appInfo.getIconImage());
@@ -625,28 +625,31 @@ public class LauncherUtility {
         appImageView.setTag(String.valueOf(appInfo.getApp().getId()));
         appImageView.setOnDragListener(new GAppDragger());
 
-        if(currentUser.getRole() == Profile.Roles.GUARDIAN)
-            appImageView.setOnClickListener(new ProfileLauncher());
-        else{
-            appImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AppInfo app = HomeActivity.getAppInfo((String) v.getTag());
-                    Intent intent = new Intent(Intent.ACTION_MAIN);
-                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                    intent.setComponent(new ComponentName(app.getPackage(), app.getActivity()));
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                            | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        if(listener == null)
+        {
+            if(currentUser.getRole() == Profile.Roles.GUARDIAN)
+                appImageView.setOnClickListener(new ProfileLauncher());
+            else{
+                appImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AppInfo app = HomeActivity.getAppInfo((String) v.getTag());
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                        intent.setComponent(new ComponentName(app.getPackage(), app.getActivity()));
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
 
-                    intent.putExtra(Constants.CHILD_ID, currentUser.getId());
-                    intent.putExtra(Constants.APP_COLOR, app.getBgColor());
-                    intent.putExtra(Constants.APP_PACKAGE_NAME, app.getPackage());
-                    intent.putExtra(Constants.APP_ACTIVITY_NAME, app.getActivity());
+                        intent.putExtra(Constants.CHILD_ID, currentUser.getId());
+                        intent.putExtra(Constants.APP_COLOR, app.getBgColor());
+                        intent.putExtra(Constants.APP_PACKAGE_NAME, app.getPackage());
+                        intent.putExtra(Constants.APP_ACTIVITY_NAME, app.getActivity());
 
-                    // Verify the intent will resolve to at least one activity
-                    LauncherUtility.secureStartActivity(v.getContext(), intent);
-                }
-            });
+                        // Verify the intent will resolve to at least one activity
+                        LauncherUtility.secureStartActivity(v.getContext(), intent);
+                    }
+                });
+            }
         }
 
         return appImageView;
