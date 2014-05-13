112ca6f61064e5df3993a788ae22ad455dd9bc94
Stefan Thilemann <thilemann@gmail.com>
2014-05-13 17:20:17 +0200

    Finished documenting class and its methods

--- src/dk/aau/cs/giraf/launcher/settings/SettingsListFragment.java
+++ src/dk/aau/cs/giraf/launcher/settings/SettingsListFragment.java
@@ -3,6 +3,7 @@ package dk.aau.cs.giraf.launcher.settings;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.sax.StartElementListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -77,22 +78,30 @@ public class SettingsListFragment extends Fragment {
         mLoggedInGuardian = pc.getProfileById(getActivity().getIntent().getIntExtra(Constants.GUARDIAN_ID, -1));
 
         int childID = getActivity().getIntent().getIntExtra(Constants.CHILD_ID, -1);
+
+        // The childID is -1 meaning that no childs are available
         if(childID == -1)
         {
             mCurrentUser = pc.getProfileById(getActivity().getIntent().getIntExtra(Constants.GUARDIAN_ID, -1));
             mProfileSelector = new GProfileSelector(getActivity(), mLoggedInGuardian, null);
         }
+        // A child is found - set it as active and add its profile selector
         else
         {
             mCurrentUser = pc.getProfileById(childID);
             mProfileSelector = new GProfileSelector(getActivity(), mLoggedInGuardian, mCurrentUser);
         }
+        // Notify about the current user
         mCallback.setCurrentUser(mCurrentUser);
+        // Update the name of the user
         mProfileName.setText(mCurrentUser.getName());
 
+        // Instantiates a new adapter to render the items in the ListView with a list of installed (available) apps
         mAdapter = new SettingsListAdapter(getActivity(), mSettingsListView, mCallback.getInstalledSettingsApps());
+        // Set the new adapter in the ListView
         mSettingsListView.setAdapter(mAdapter);
 
+        // Set listerners for loaded user interface components
         setListeners();
 
         return view;
@@ -114,38 +123,55 @@ public class SettingsListFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        // Set the selection in the adapter when the activity is created/reloaded
+        // This is important when reloading the activity to show settings for
+        // selected user.
         mAdapter.setSelected(0);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        // If the activity should be garbage collected, the adapter should retain its selection
         mAdapter.setSelected(0);
     }
 
+    /**
+     * Sets the listeners to be used by the components in SettingsListFragment.
+     * Called in onCreate.
+     */
     private void setListeners() {
+        // Handles a clicked row in the ListView
         mSettingsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 SettingsListItem item = (SettingsListItem) parent.getAdapter().getItem(position);
+                // If the item contains a fragment, set it as active
                 if (item.mAppFragment != null) {
+                    // Notify class implementing the callback interface that a new fragment has been selected
                     mCallback.setActiveFragment(item.mAppFragment);
+                    // Update the adapter to reflect the selection
                     mAdapter.setSelected(position);
                 }
+                // Otherwise it must be an intent
                 else if (item.mIntent != null) {
+                    // Start a new activity with the intent
+                    // No need to reflect this as a selection in the adapter, since it is starting an activity
                     startActivity(item.mIntent);
-                    mAdapter.setSelected(0);
                 }
             }
         });
 
+        // Handles a click on the profile selector
         mProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Show the profile select dialog
                 mProfileSelector.show();
             }
         });
 
+        // Set listerner for the dialog
         setProfileSelectorClickListener();
     }
 
@@ -155,21 +181,28 @@ public class SettingsListFragment extends Fragment {
      * */
     private void setProfileSelectorClickListener()
     {
+        // Handles a selection returned from the profile select dialog
         mProfileSelector.setOnListItemClick(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 ProfileController pc = new ProfileController(getActivity());
+                // Get a profile from id returned from dialog
                 mCurrentUser = pc.getProfileById((int) id);
+                // Close it when a selection has been made
                 mProfileSelector.dismiss();
 
+                // Set a new profile select button according to the role of active user
                 if (mCurrentUser.getRole() != Profile.Roles.GUARDIAN)
                     mProfileSelector = new GProfileSelector(getActivity(), mLoggedInGuardian, mCurrentUser);
                 else
                     mProfileSelector = new GProfileSelector(getActivity(), mLoggedInGuardian, null);
 
+                // Notify that a profile selection has been made
                 mCallback.setCurrentUser(mCurrentUser);
+                // Reload activity to reflect different user settings
                 mCallback.reloadActivity();
 
+                // Call this method again to set listerners for the newly selected profile
                 setProfileSelectorClickListener();
             }
         });
