321314b4e0a5aa4cdd23aa48326af745918559a2
Tonaplo <tonaplo@msn.com>
2014-05-18 12:28:19 +0200

    Commented SimulateAnimationDrawable completely

--- launcher-app/src/dk/aau/cs/giraf/launcher/layoutcontroller/SimulateAnimationDrawable.java
+++ launcher-app/src/dk/aau/cs/giraf/launcher/layoutcontroller/SimulateAnimationDrawable.java
@@ -2,12 +2,27 @@ package dk.aau.cs.giraf.launcher.layoutcontroller;
 
 import android.widget.ImageView;
 
+/**
+ * This class simulates the animation in AuthenticationActivity, scanning the QR code.
+ * The animation itself is simply 96 pictures shown in sequence, which this class handles.
+ */
 public class SimulateAnimationDrawable{
+    
+    /**
+     * the variables needed by the class.
+     * The ImageView then animation plays in, the index of the frames, the duration between each frame and the index of the last frame.
+     */
     private ImageView mImageView;
     private int[] mFrames;
     private int mDuration;
     private int mLastFrameNo;
 
+    /**
+     * The constructor for the class
+     * @param pImageView The ImageView we are simulating the class in.
+     * @param pFrames The index of the frames to be shown
+     * @param pDuration The duration that each frame should be shown.
+     */
     public SimulateAnimationDrawable(ImageView pImageView, int[] pFrames, int pDuration){
         mImageView = pImageView;
         mFrames = pFrames;
@@ -18,6 +33,10 @@ public class SimulateAnimationDrawable{
         play(1);
     }
 
+    /**
+     * The play function simulates the animation by itself.
+     * @param pFrameNo The frame number to start with. This is usually called with 1 as argument.
+     */
     private void play(final int pFrameNo){
         mImageView.postDelayed(new Runnable(){
             @Override
