707494a0c55df1258fff388ab11aff8e9c5fd900
Tonaplo <tonaplo@msn.com>
2014-05-18 13:15:30 +0200

    Commented the SettingsLauncher class completely

--- launcher-app/src/dk/aau/cs/giraf/launcher/settings/SettingsLauncher.java
+++ launcher-app/src/dk/aau/cs/giraf/launcher/settings/SettingsLauncher.java
@@ -9,14 +9,27 @@ import dk.aau.cs.giraf.launcher.R;
 import dk.aau.cs.giraf.launcher.settings.SettingsUtility;
 import dk.aau.cs.giraf.launcher.settings.components.IconResizer;
 
+/**
+ * This fragment contains the settings for Launcher itself, meaning the iconsize and
+ * whether to show the starting animation or not.
+ */
 public class SettingsLauncher extends PreferenceFragment {
 
     private String userPart;
 
+    /**
+     * The contructor for the class
+     * @param userPart The userpart of the sharedpreferences
+     */
     public SettingsLauncher(String userPart){
         this.userPart = userPart;
     }
 
+    /**
+     * OnCreate is per usual overridden to instantiate the mayority of the variables of the class
+     * It loads the SharedPreferences of the user and sets the layout accordingly.
+     * @param savedInstanceState The previously SavedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
