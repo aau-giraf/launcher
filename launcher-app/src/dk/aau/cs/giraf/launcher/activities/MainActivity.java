2b51e75875b16797315eb2efd7cfd1665576ad76
Stefan Thilemann <thilemann@gmail.com>
2014-05-19 10:27:16 +0200

    Sets DEBUG_MODE to false.

--- launcher-app/src/dk/aau/cs/giraf/launcher/activities/MainActivity.java
+++ launcher-app/src/dk/aau/cs/giraf/launcher/activities/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity implements Animation.AnimationListene
      * If {@code true}, the Launcher is stated in debugging mode, where the splash screen and
      * authentication_activity is skipped.
      */
-    private final boolean DEBUG_MODE = true;
+    private final boolean DEBUG_MODE = false;
 
     /**
      * If {@code true}, the authentication_activity screen is shown, despite debugging mode. Has no
