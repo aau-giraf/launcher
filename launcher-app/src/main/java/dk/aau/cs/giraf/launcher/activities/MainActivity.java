c7612808b580486ecd2936601918cdf1a96dfd88
Niklas Kirk Mouritzsen <nmouri12@student.aau.dk>
2015-04-30 10:52:27 +0200

    Increate the timeout for when the user has internet or not

--- launcher-app/src/main/java/dk/aau/cs/giraf/launcher/activities/MainActivity.java
+++ launcher-app/src/main/java/dk/aau/cs/giraf/launcher/activities/MainActivity.java
@@ -296,7 +296,7 @@ public class MainActivity extends Activity implements Animation.AnimationListene
                                 HttpURLConnection urlc = (HttpURLConnection) new URL("http://clients3.google.com/generate_204").openConnection();
                                 urlc.setRequestProperty("User-Agent", "Android");
                                 urlc.setRequestProperty("Connection", "close");
-                                urlc.setConnectTimeout(5);
+                                urlc.setConnectTimeout(1500);
                                 urlc.connect();
                                 hasConnectionTemp = (urlc.getResponseCode() == 204 && urlc.getContentLength() == 0);
                             } catch (IOException e) {
