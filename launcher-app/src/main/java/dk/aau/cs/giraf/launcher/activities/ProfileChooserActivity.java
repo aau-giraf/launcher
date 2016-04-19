f1dc00191c5a0fec380520498295f7db322703d3
nsmed13 <nsmed13@student.aau.dk>
2016-04-19 10:07:41 +0200

    Made stuff work

--- launcher-app/src/main/java/dk/aau/cs/giraf/launcher/activities/ProfileChooserActivity.java
+++ launcher-app/src/main/java/dk/aau/cs/giraf/launcher/activities/ProfileChooserActivity.java
@@ -1,7 +1,9 @@
 package dk.aau.cs.giraf.launcher.activities;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.SyncRequest;
 import android.content.SyncStatusObserver;
 import android.database.Cursor;
 import android.net.Uri;
@@ -16,17 +18,21 @@ import dk.aau.cs.giraf.launcher.R;
 import dk.aau.cs.giraf.launcher.helper.SwipeAdapter;
 import dk.aau.cs.giraf.librest.ContentProvider;
 import dk.aau.cs.giraf.librest.User;
+import dk.aau.cs.giraf.librest.UserService;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 /**
  * Created by Caspar on 10-03-2016.
  */
 public class ProfileChooserActivity extends Activity {
-    private static final Uri userURI = Uri.parse("http://web.giraf.cs.aau.dk:8080/giraf-rest_service-1.0.0/user");
+    private static final Uri userURI = Uri.parse("content://dk.aau.cs.giraf.provider.Users/users");
 
     ViewPager viewPager;
     SwipeAdapter adapter;
 
     Object handleSyncObserver;
+    AccountManager accountManager;
 
     SyncStatusObserver syncObserver = new SyncStatusObserver() {
         @Override
@@ -45,12 +51,16 @@ public class ProfileChooserActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_chooser);
 
-        Cursor c = getContentResolver().query(userURI,null,null,null,null,null);
+        accountManager = AccountManager.get(this);
+
+        ContentResolver.requestSync();
+
+        Cursor c = getContentResolver().query(userURI, null, null, null, null);
         List<User> users = new LinkedList<User>();
         while(c.moveToNext()){
             users.add(User.fromCursor(c));
         }
-                viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
         adapter = new SwipeAdapter(this);
         viewPager.setAdapter(adapter);
     }
@@ -58,17 +68,17 @@ public class ProfileChooserActivity extends Activity {
     @Override
     protected void onPause() {
         if(handleSyncObserver != null){
-            ContentResolver.removeStatusChangeListener(handleSyncObserver);
+            getContentResolver().removeStatusChangeListener(handleSyncObserver);
         }
-        super.onStop();
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        handleSyncObserver = ContentResolver.addStatusChangeListener(
-                ContentResolver.SYNC_OBSERVER_TYPE_ACTIVE |
-                ContentResolver.SYNC_OBSERVER_TYPE_PENDING, syncObserver);
+        handleSyncObserver = getContentResolver().addStatusChangeListener(
+                getContentResolver().SYNC_OBSERVER_TYPE_ACTIVE |
+                getContentResolver().SYNC_OBSERVER_TYPE_PENDING, syncObserver);
     }
 
     //Use to update status depending on if the ContentProvider is Syncing
