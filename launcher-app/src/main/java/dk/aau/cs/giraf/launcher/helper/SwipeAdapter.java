1b31c2a211779241900ee873cb1e35a98271ffde
Caspar <ckucha13@student.aau.dk>
2016-04-28 09:26:12 +0200

    Profile screen changes.
    Added 2 more profiles to the screen, now there can be 10x profiles on a
    screen.
    Added some resources for the profile screen.
    Added dependencies on Picasso plugin to allow image manipulation in the
    swipe adapter.
    
    @MISC{TwistTeam,
      author = {Twist Team},
      title = {Guide Test Automation},
      month = {},
      year = {},
      url = {https://www.thoughtworks.com/insights/blog/guide-test-automation},
      lastchecked = {26-04-2016},
    }

--- launcher-app/src/main/java/dk/aau/cs/giraf/launcher/helper/SwipeAdapter.java
+++ launcher-app/src/main/java/dk/aau/cs/giraf/launcher/helper/SwipeAdapter.java
@@ -2,19 +2,28 @@ package dk.aau.cs.giraf.launcher.helper;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import dk.aau.cs.giraf.launcher.R;
 import dk.aau.cs.giraf.librest.provider.GirafProvider;
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 /**
  * Created by Caspar on 10-03-2016.
@@ -23,15 +32,21 @@ public class SwipeAdapter extends PagerAdapter {
     private List<Integer> mImages;
     private List<String> mUsers;
     private LayoutInflater layoutInflater;
-    private static int NUM_OF_PROFILES_PR_PAGE = 8;
+    private static int NUM_OF_PROFILES_PR_PAGE = 10;
     private Cursor cursor;
+    private Context mContext;
 
-    private int[] imageFields = {R.id.image_view1,R.id.image_view2,R.id.image_view3,R.id.image_view4,R.id.image_view5,R.id.image_view6,R.id.image_view7, R.id.image_view8};
-    private int[] textFields = {R.id.image_text1,R.id.image_text2,R.id.image_text3,R.id.image_text4,R.id.image_text5,R.id.image_text6,R.id.image_text7,R.id.image_text8};
+    private final int radius = 5;
+    private final int margin = 5;
+    private Transformation transformation = new RoundedCornersTransformation(radius,margin);
+
+    private int[] imageFields = {R.id.image_view1,R.id.image_view2,R.id.image_view3,R.id.image_view4,R.id.image_view5,R.id.image_view6,R.id.image_view7, R.id.image_view8,R.id.image_view9,R.id.image_view10};
+    private int[] textFields = {R.id.image_text1,R.id.image_text2,R.id.image_text3,R.id.image_text4,R.id.image_text5,R.id.image_text6,R.id.image_text7,R.id.image_text8,R.id.image_text9,R.id.image_text10};
 
     public SwipeAdapter(Context context, Cursor cursor){
         this.cursor = cursor;
         this.layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        this.mContext = context;
 
         mImages = new LinkedList<Integer>();
         mUsers = new LinkedList<String>();
@@ -73,7 +88,7 @@ public class SwipeAdapter extends PagerAdapter {
 
         cursor.moveToPosition(nextProfile);
 
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < 10; i++){
             if(nextProfile >= cursor.getCount()){
                 break;
             }
@@ -82,6 +97,12 @@ public class SwipeAdapter extends PagerAdapter {
 
             imageView.setImageResource(mImages.get(nextProfile));
             int columnIndex = cursor.getColumnIndex(GirafProvider.USERNAME);
+
+            Picasso.with(mContext).load(mImages.get(nextProfile))
+                    .transform(transformation)
+                    .placeholder(R.drawable.bg)
+                    .into(imageView);
+
             textView.setText(cursor.getString(columnIndex));
 
             System.out.println("Next Profile number: " + nextProfile);
