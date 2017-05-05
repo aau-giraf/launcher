cc40a74e8601981dc48d032f29a148077f3a197e
Christoffer Mourtizen <cmouri14@student.aau.dk>
2017-05-05 11:34:18 +0200

    Small changes and codestyle to login controller and activity. Rename authentication_* layout and string xml file to login_*.

--- launcher-app/src/main/java/dk/aau/cs/giraf/launcher/activities/LoginActivity.java
+++ launcher-app/src/main/java/dk/aau/cs/giraf/launcher/activities/LoginActivity.java
@@ -10,14 +10,12 @@ import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import dk.aau.cs.giraf.activity.GirafActivity;
 import dk.aau.cs.giraf.launcher.R;
 import dk.aau.cs.giraf.launcher.helper.Constants;
 import dk.aau.cs.giraf.launcher.logiccontroller.LoginController;
-import dk.aau.cs.giraf.utilities.GrayScaleHelper;
 
-public class LoginActivity extends GirafActivity{
+public class LoginActivity extends GirafActivity {
 
     private LoginController controller;
     private TextView usernameTextBox;
@@ -25,20 +23,17 @@ public class LoginActivity extends GirafActivity{
     private Button loginButton;
     private Animation loadAnimation;
 
-/*
-* Constructer
-*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setTrackingId("UA-48608499-1");
         controller = new LoginController(this);
-        setContentView(R.layout.authentication_activity2);
+        setContentView(R.layout.login_activity);
         loadAnimation = AnimationUtils.loadAnimation(this, R.anim.main_activity_rotatelogo_infinite);
         loadAnimation.setDuration(Constants.LOGO_ANIMATION_DURATION);
-        usernameTextBox = (TextView) findViewById(R.id.username_box);
-        passwordTextBox = (TextView) findViewById(R.id.pass_box);
-        loginButton = (Button) findViewById(R.id.login_btn);
+        usernameTextBox = (TextView) findViewById(R.id.username_textbox);
+        passwordTextBox = (TextView) findViewById(R.id.password_textbox);
+        loginButton = (Button) findViewById(R.id.login_button);
         findViewById(android.R.id.content).setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -46,13 +41,9 @@ public class LoginActivity extends GirafActivity{
                 return false;
             }
         });
-
     }
 
 
-/*
-* Override methods
-*/
     /**
      * Does nothing, to prevent the user from returning to the splash screen or native OS.
      */
@@ -61,22 +52,28 @@ public class LoginActivity extends GirafActivity{
         //Do nothing, as the user should not be able to back out of this activity
     }
 
-/*
-* Gui methods
-*/
-    public void loginBtnPressed(View view){
+
+    /**
+     * Called from login_activity.xml using the onClick event of the login button.
+     * @param view the view.
+     */
+    public void onLoginButtonClick(View view) {
         loginButton.setEnabled(false);
-        String username = usernameTextBox.getText().toString();
-        String password = passwordTextBox.getText().toString();
         usernameTextBox.setEnabled(false);
         passwordTextBox.setEnabled(false);
+        String username = usernameTextBox.getText().toString();
+        String password = passwordTextBox.getText().toString();
         findViewById(R.id.girafHeaderIcon).startAnimation(loadAnimation);
         controller.login(username, password);
     }
 
 
-
-    public void showDialogWithMessage(String message){
+    /**
+     * Shows a dialog with a message.
+     * ToDo change to giraf dialog when it is finished.
+     * @param message the message.
+     */
+    public void showDialogWithMessage(String message) {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this,R.style.GirafTheme);
         dialog.setTitle(R.string.error_login);
         dialog.setMessage(message);
@@ -85,20 +82,20 @@ public class LoginActivity extends GirafActivity{
             public void onClick(DialogInterface dialogInterface, int id) {
                 findViewById(R.id.girafHeaderIcon).clearAnimation();
                 dialogInterface.cancel();
-                ReEnableGuiControls();
+                reEnableGuiControls();
             }
         });
         dialog.show();
     }
 
 
-    public void hideSoftKeyboard() {
-        InputMethodManager inputMethodManager = (InputMethodManager)  this.getSystemService(this.INPUT_METHOD_SERVICE);
+    private void hideSoftKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager)  this.getSystemService(INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
     }
 
 
-    private void ReEnableGuiControls(){
+    private void reEnableGuiControls() {
         loginButton.setEnabled(true);
         usernameTextBox.setEnabled(true);
         passwordTextBox.setEnabled(true);
--- launcher-app/src/main/java/dk/aau/cs/giraf/launcher/logiccontroller/LoginController.java
+++ launcher-app/src/main/java/dk/aau/cs/giraf/launcher/logiccontroller/LoginController.java
@@ -1,21 +1,17 @@
 package dk.aau.cs.giraf.launcher.logiccontroller;
 
 import android.content.Intent;
+import com.android.volley.*;
 
-
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import dk.aau.cs.giraf.librest.requests.LoginRequest;
-import dk.aau.cs.giraf.librest.requests.GetRequest;
-import dk.aau.cs.giraf.librest.requests.RequestQueueHandler;
-import dk.aau.cs.giraf.models.core.User;
-
+import dk.aau.cs.giraf.launcher.R;
 import dk.aau.cs.giraf.launcher.activities.HomeActivity;
 import dk.aau.cs.giraf.launcher.activities.LoginActivity;
 import dk.aau.cs.giraf.launcher.helper.Constants;
 import dk.aau.cs.giraf.launcher.helper.LauncherUtility;
-
+import dk.aau.cs.giraf.librest.requests.GetRequest;
+import dk.aau.cs.giraf.librest.requests.LoginRequest;
+import dk.aau.cs.giraf.librest.requests.RequestQueueHandler;
+import dk.aau.cs.giraf.models.core.User;
 
 import java.util.Date;
 
@@ -29,21 +25,22 @@ public class LoginController {
     }
 
     /**
-     * Verifies a username and password and if correct logs in with the information
+     * Verifies a username and password and if correct logs in with the information.
      *
-     * @param username The users username
-     * @param password The users password
+     * @param username The users username.
+     * @param password The users password.
      */
     public void login(final String username, String password) {
         queue = RequestQueueHandler.getInstance(gui.getApplicationContext()).getRequestQueue();
 
         //Creates a login request which is then added to the queue later
         LoginRequest loginRequest = new LoginRequest(username, password,
-                new Response.Listener<Integer>() {
-                    @Override
-                    public void onResponse(Integer statusCode) {
-                        //Creates a GetRequest for the user, which is then added to the queue within the loginRequest
-                        GetRequest<User> userGetRequest = new GetRequest<User>(username, User.class, new Response.Listener<User>() {
+            new Response.Listener<Integer>() {
+                @Override
+                public void onResponse(Integer statusCode) {
+                    //Creates a GetRequest for the user, which is then added to the queue within the loginRequest
+                    GetRequest<User> userGetRequest =
+                        new GetRequest<User>(username, User.class, new Response.Listener<User>() {
                             //Passes the userinfo to homeIntent
                             @Override
                             public void onResponse(User response) {
@@ -55,23 +52,28 @@ public class LoginController {
                                 gui.startActivity(homeIntent);
                             }
                         }, new Response.ErrorListener() {
-                            //The user is for some reason unavailable
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                //ToDo Localise me later
-                                gui.showDialogWithMessage("try again later");
+                                //The user is for some reason unavailable
+                                gui.showDialogWithMessage(gui.getString(R.string.error_try_agian));
                             }
                         });
-                        queue.add(userGetRequest);
-                    }
-                },
-                new Response.ErrorListener(){
-                    @Override
-                    //The username and/or password are incorrect
-                    public void onErrorResponse(VolleyError error) {
-                        gui.showDialogWithMessage("temp fejl msg");
+                    queue.add(userGetRequest);
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    int errorCode = error.networkResponse.statusCode;
+                    if (errorCode == 401) {
+                        //The username and/or password are incorrect
+                        gui.showDialogWithMessage(gui.getString(R.string.error_username_password));
+                    } else {
+                        //The server is for some reason unavailable
+                        gui.showDialogWithMessage(gui.getString(R.string.error_try_agian));
                     }
                 }
+            }
         );
         //The login request is added to the queue
         queue.add(loginRequest);
